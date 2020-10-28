da6e5961858dffd381ee519a46b551f088df2181
pangda <sun-ming-da@163.com>
2020-10-28 23:32:57 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <br />
+      <a href="../src/views/demo.html" target="blank">demo</a>
     </div>
     <router-view />
   </div>
