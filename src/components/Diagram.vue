96ddf44b7c00b932990a62b7a92a12cb521515d8
sunmd <sun-ming-da@163.com>
2020-10-30 17:30:54 +0800

    to(template): 进行vue的封装

--- src/components/Diagram.vue
+++ src/components/Diagram.vue
@@ -1,1 +1,237 @@
+<template>
+  <div
+    id="myDiagramDiv"
+    style="width:1024px; height:720px; background-color: #002b58;"
+  ></div>
+</template>
 
+<script>
+import go from "gojs";
+export default {
+  props: {
+    moduleData: {
+      required: true,
+      type: Array
+    }
+  },
+  mounted: function() {
+    // 定义对应的$符号
+    const $ = go.GraphObject.make;
+    // 定义图片div
+    let myDiagramDiv = $(go.Diagram, "myDiagramDiv", {
+      "undoManager.isEnabled": false
+    });
+
+    // 定义默认的模版样式
+    const myNodeTemplate = $(
+      go.Node,
+      "Auto",
+      {},
+      // 对话框
+      $(go.Shape, "RoundedRectangle", { strokeWidth: 0, fill: "#004378" }),
+      $(
+        go.Panel,
+        "Table",
+        { defaultAlignment: go.Spot.Left },
+
+        // 电话号码行
+        $(
+          go.Panel,
+          "Horizontal",
+          { margin: 6 },
+          { row: 0, column: 0 },
+          $(go.TextBlock, "号码:", {
+            textAlign: "left",
+            margin: 0,
+            stroke: "white"
+          }),
+          $(
+            go.TextBlock,
+            "",
+            { textAlign: "left", margin: 0, stroke: "white" },
+            new go.Binding("text", "tel")
+          )
+        ),
+        $(
+          go.TextBlock,
+          "查看图谱",
+          { textAlign: "left", margin: 6, stroke: "white" },
+          { row: 0, column: 2 }
+        ),
+
+        // 姓名行
+        $(
+          go.Panel,
+          "Horizontal",
+          { margin: 6 },
+          { row: 1, column: 0 },
+          $(go.TextBlock, "姓名:", {
+            textAlign: "left",
+            margin: 0,
+            stroke: "white"
+          }),
+          $(
+            go.TextBlock,
+            "",
+            { textAlign: "left", margin: 0, stroke: "white" },
+            new go.Binding("text", "name")
+          )
+        ),
+
+        // 身份证行
+        $(
+          go.Panel,
+          "Horizontal",
+          { margin: 6 },
+          { row: 2, column: 0 },
+          $(go.TextBlock, "身份证号码:", {
+            textAlign: "left",
+            margin: 0,
+            stroke: "white"
+          }),
+          $(
+            go.TextBlock,
+            "",
+            { textAlign: "left", margin: 0, stroke: "white" },
+            new go.Binding("text", "id")
+          )
+        ),
+
+        // 通联人数行
+        $(
+          go.Panel,
+          "Horizontal",
+          { margin: 6 },
+          { row: 3, column: 0 },
+          $(go.TextBlock, "通联人数:", {
+            textAlign: "left",
+            margin: 0,
+            stroke: "white"
+          }),
+          $(
+            go.TextBlock,
+            "",
+            { textAlign: "left", margin: 0, stroke: "white" },
+            new go.Binding("text", "linkNum")
+          )
+        )
+      )
+    );
+
+    // 添加行首的模版样式
+    let totalTemplate = $(
+      go.Node,
+      "Auto",
+      {},
+      // 对话框
+      $(go.Shape, "RoundedRectangle", { strokeWidth: 0, fill: "#004378" }),
+      $(
+        go.Panel,
+        "Table",
+        { defaultAlignment: go.Spot.Left },
+
+        // 首行加颜色
+        $(
+          go.Panel,
+          "TableRow",
+          { background: "#063a73" },
+          $(
+            go.Panel,
+            "Horizontal",
+            { margin: 6 },
+            { row: 0, column: 0 },
+            $(go.TextBlock, "目标:", {
+              textAlign: "left",
+              margin: 0,
+              stroke: "white"
+            }),
+            $(
+              go.TextBlock,
+              "",
+              { textAlign: "left", margin: 0, stroke: "#a5507b" },
+              new go.Binding("text", "totalNum")
+            ),
+            $(go.TextBlock, "个", {
+              textAlign: "left",
+              margin: 0,
+              stroke: "white"
+            })
+          )
+        ),
+        $(
+          go.TextBlock,
+          "查看图谱",
+          { textAlign: "left", margin: 6, stroke: "#5f9bda" },
+          { row: 0, column: 2 }
+        ),
+
+        // 任务名称
+        $(
+          go.TextBlock,
+          "",
+          { margin: 6, stroke: "white", row: 1, column: 0 },
+          new go.Binding("text", "mission")
+        ),
+
+        //话单行
+        $(
+          go.Panel,
+          "Horizontal",
+          { margin: 6 },
+          { row: 2, column: 0 },
+          $(go.TextBlock, "话单数:", { margin: 0, stroke: "white" }),
+          $(
+            go.TextBlock,
+            "",
+            { margin: 0, stroke: "white" },
+            new go.Binding("text", "totalTel")
+          )
+        ),
+
+        $(
+          go.Panel,
+          "Horizontal",
+          { margin: 6 },
+          { row: 3, column: 0 },
+          $(go.TextBlock, "节点数:", { margin: 0, stroke: "white" }),
+          $(
+            go.TextBlock,
+            "",
+            { margin: 0, stroke: "white" },
+            new go.Binding("text", "nodeId")
+          )
+        )
+      )
+    );
+
+    // 使用模版map
+    let templmap = new go.Map();
+    templmap.add("", myNodeTemplate);
+    templmap.add("total", totalTemplate);
+
+    myDiagramDiv.nodeTemplateMap = templmap;
+
+    // 使用树状布局
+    myDiagramDiv.layout = $(go.TreeLayout, { nodeSpacing: 3 });
+
+    // 链接线设定
+    // Define a trivial link template with no arrowhead
+    myDiagramDiv.linkTemplate = $(
+      go.Link,
+      {
+        curve: go.Link.Bezier,
+        toEndSegmentLength: 30,
+        fromEndSegmentLength: 30
+      },
+      $(go.Shape, { strokeWidth: 1.5, stroke: "#57728f" }) // the link shape, with the default black stroke
+    );
+
+    // 树状数据
+    let myModel = $(go.TreeModel);
+    myModel.nodeDataArray = this.moduleData;
+    myDiagramDiv.model = myModel;
+  }
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,127 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Diagram :moduleData="moduleData" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Diagram from "@/components/Diagram";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Diagram
+  },
+  data() {
+    return {
+      moduleData: [
+        {
+          key: "1",
+          mission: "任务名称1任务名称1任务名称1",
+          totalTel: "4556677",
+          nodeId: "2220",
+          totalNum: "24",
+          category: "total"
+        },
+        {
+          key: "2",
+          parent: "1",
+          name: "Demeter",
+          tel: "13844556678",
+          id: "2202220222202220",
+          linkNum: "24人"
+        },
+        {
+          key: "3",
+          parent: "1",
+          name: "Copricat",
+          tel: "13844556679",
+          id: "2202220222202220",
+          linkNum: "24人"
+        },
+        {
+          key: "4",
+          parent: "1",
+          name: "Jellylorum",
+          tel: "13844556680",
+          id: "2202220222202220",
+          linkNum: "24人"
+        },
+        {
+          key: "5",
+          parent: "1",
+          name: "Alonzo",
+          tel: "13844556681",
+          id: "2202220222202220",
+          linkNum: "24人"
+        },
+        {
+          key: "6",
+          parent: "1",
+          name: "Alonzo",
+          tel: "13844556682",
+          id: "2202220222202220",
+          linkNum: "24人"
+        },
+        {
+          key: "7",
+          parent: "1",
+          name: "Alonzo",
+          tel: "13844556683",
+          id: "2202220222202220",
+          linkNum: "24人"
+        },
+        {
+          key: "8",
+          parent: "2",
+          name: "Munkustrap",
+          tel: "13844556684",
+          id: "2202220222202220",
+          linkNum: "24人"
+        },
+        {
+          key: "9",
+          parent: "2",
+          name: "Munkustrap",
+          tel: "13844556685",
+          id: "2202220222202220",
+          linkNum: "24人"
+        },
+        {
+          key: "10",
+          parent: "2",
+          name: "Munkustrap",
+          tel: "13844556686",
+          id: "2202220222202220",
+          linkNum: "24人"
+        },
+        {
+          key: "11",
+          parent: "2",
+          name: "Munkustrap",
+          tel: "13844556687",
+          id: "2202220222202220",
+          linkNum: "24人"
+        },
+        {
+          key: "12",
+          parent: "2",
+          name: "Munkustrap",
+          tel: "13844556688",
+          id: "2202220222202220",
+          linkNum: "24人"
+        },
+        {
+          key: "13",
+          parent: "2",
+          name: "Munkustrap",
+          tel: "13844556689",
+          id: "2202220222202220",
+          linkNum: "24人"
+        }
+      ]
+    };
   }
 };
 </script>
